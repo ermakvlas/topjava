661fe4563b9500a83938cf9d92381fed05110c78
ermakvlas <mindw0rk.vlas@gmail.com>
2016-10-03 21:10:59 +0500

    HW03

--- src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcMealRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcMealRepositoryImpl.java
@@ -1,9 +1,17 @@
 package ru.javawebinar.topjava.repository.jdbc;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
+import ru.javawebinar.topjava.AuthorizedUser;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
 
+import javax.sql.DataSource;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -15,28 +23,62 @@ import java.util.List;
 @Repository
 public class JdbcMealRepositoryImpl implements MealRepository {
 
+    private static final BeanPropertyRowMapper<Meal> ROW_MAPPER = BeanPropertyRowMapper.newInstance(Meal.class);
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+    private SimpleJdbcInsert insertMeal;
+
+    public JdbcMealRepositoryImpl(DataSource dataSource) {
+        this.insertMeal = new SimpleJdbcInsert(dataSource).withTableName("MEALS").usingGeneratedKeyColumns("id");
+    }
+
     @Override
     public Meal save(Meal Meal, int userId) {
-        return null;
+        if (AuthorizedUser.id()!=userId){return null;}
+
+        MapSqlParameterSource map = new MapSqlParameterSource()
+                .addValue("id", Meal.getId())
+                .addValue("date_time", Meal.getDateTime())
+                .addValue("description", Meal.getDescription())
+                .addValue("calories", Meal.getCalories())
+                .addValue("user_id", userId);
+
+        if (Meal.isNew()){
+            Number newKey = insertMeal.executeAndReturnKey(map);
+            Meal.setId(newKey.intValue());
+        } else {
+            namedParameterJdbcTemplate.update("UPDATE meals SET date_time=:date_time, description=:description, calories=:calories, user_id=:id WHERE id=:id", map);
+        }
+        return Meal;
     }
 
     @Override
     public boolean delete(int id, int userId) {
-        return false;
+        if (AuthorizedUser.id()!=userId){return false;}
+        return jdbcTemplate.update("DELETE FROM meals WHERE id=?", id) != 0;
     }
 
     @Override
-    public Meal get(int id, int userId) {
-        return null;
+    public Meal get(int id, int userId)
+    {
+        if (AuthorizedUser.id()!=userId){return null;}
+        return jdbcTemplate.queryForObject("SELECT id, date_time, description, calories, user_id FROM meals WHERE id=?", ROW_MAPPER, id);
     }
 
     @Override
     public List<Meal> getAll(int userId) {
-        return null;
+        if (AuthorizedUser.id()!=userId){return null;}
+        return jdbcTemplate.query("SELECT * FROM meals ORDER BY date_time", ROW_MAPPER);
     }
 
     @Override
     public List<Meal> getBetween(LocalDateTime startDate, LocalDateTime endDate, int userId) {
-        return null;
+        if (AuthorizedUser.id()!=userId){return null;}
+        return jdbcTemplate.query("SELECT * FROM meals WHERE date_time>=? AND date_time<=? ORDER BY date_time", ROW_MAPPER, startDate, endDate);
     }
 }
--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,5 +1,6 @@
 DELETE FROM user_roles;
 DELETE FROM users;
+DELETE FROM meals;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
 INSERT INTO users (name, email, password)
@@ -11,3 +12,21 @@ VALUES ('Admin', 'admin@gmail.com', 'admin');
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
+
+INSERT INTO meals (id, date_time, description, calories, user_id)
+VALUES (1, '2015-05-30 10:00:00.000000', 'Завтрак', 500, 100000);
+
+INSERT INTO meals (id, date_time, description, calories, user_id)
+VALUES (2, '2015-05-30 13:00:00.000000', 'Обед', 1000, 100000);
+
+INSERT INTO meals (id, date_time, description, calories, user_id)
+VALUES (3, '2015-05-30 20:00:00.000000', 'Ужин', 500, 100000);
+
+INSERT INTO meals (id, date_time, description, calories, user_id)
+VALUES (4, '2015-05-31 10:00:00.000000', 'Завтрак', 1000, 100001);
+
+INSERT INTO meals (id, date_time, description, calories, user_id)
+VALUES (5, '2015-05-31 13:00:00.000000', 'Обед', 500, 100001);
+
+INSERT INTO meals (id, date_time, description, calories, user_id)
+VALUES (6, '2015-05-31 20:00:00.000000', 'Ужин', 510, 100001);
