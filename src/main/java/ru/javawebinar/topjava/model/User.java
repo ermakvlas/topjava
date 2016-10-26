45aefb03384ce72239c487def7ea262e03a5059f
ermakvlas <mindw0rk.vlas@gmail.com>
2016-10-26 16:35:42 +0500

    lesson 7

--- src/main/java/ru/javawebinar/topjava/model/User.java
+++ src/main/java/ru/javawebinar/topjava/model/User.java
@@ -63,6 +63,7 @@ public class User extends NamedEntity {
 
     @OneToMany(cascade = CascadeType.REMOVE, fetch = FetchType.LAZY, mappedBy = "user")
     @OrderBy("dateTime DESC")
+//    @JsonIgnore
     protected List<Meal> meals;
 
     public User() {
